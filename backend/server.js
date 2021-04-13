f91e8680ceec97a082ff115a5cf7a731e5801470
Jaehoon Han <JaehoonHan1@gmail.com>
2021-04-13 04:04:17 -0400

    Fix file path bug at API routs

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,32 @@
-
+// Require packages
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+// Configure environment variables
 require('dotenv').config();
-
+// Express server
 const app = express();
 const port = process.env.PORT || 5000;
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB URI
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true}); //useUnifiedTopology: true
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+// Require models
 const exercsisesRouter = require('./routes/exercises');
-const usersRouter = require('./routers/users');
+const usersRouter = require('./routes/users');
+// Use
+app.use('/exercises', exercsisesRouter);
+app.use('/users', usersRouter);
 
-app.use('./exercises', exercsisesRouter);
-app.use('./users', usersRouter);
-
+// Start server 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
