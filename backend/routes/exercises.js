4b1b8a861d9f4b851f8adb88d4bd4c9dc6ef678d
Jaehoon Han <JaehoonHan1@gmail.com>
2021-04-17 02:52:30 -0400

    Add delete,update,get endpoints for exercises

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -13,7 +13,7 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// GET Request
+// POST create exercise
 //rooturl/exercises/add
 router.route('/add').post((req, res) => {
 
@@ -32,6 +32,35 @@ router.route('/add').post((req, res) => {
     // .save() mongoose command
     newExercise.save()
         .then(() => res.json('Exercise added.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// GET read exercise object by id
+router.route('/:id').get((req,res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json('Error : ' + err));
+});
+
+// DELETE exercise object by id
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+// POST update exercise by object id
+router.route('/update/:id').post((req,res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise.save()
+                .then(() => res.json('Exercise updated!'))
+                .catch(err => res.status(400).json('Error: ' + err))
+        })
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
