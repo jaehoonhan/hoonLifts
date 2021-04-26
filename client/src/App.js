9bf6f7368cd2911c023fefca524e85ed282c880a
Jaehoon Han <JaehoonHan1@gmail.com>
2021-04-26 19:54:02 -0400

    add navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
 function App() {
   return (
     <Router>
       <Navbar />
       <br/>
+      {/* map url paths */}
       <Route path="/" exact component={ExerciseList} />
       <Route path="/edit/:id" exact component={EditExercise} />
       <Route path="/create" exact component={CreateExercise} />
