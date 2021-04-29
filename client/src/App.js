3c53a84dd69a18e8becfdb95ea3a9fa7d81106bd
Jaehoon Han <JaehoonHan1@gmail.com>
2021-04-28 22:54:37 -0400

    Fix typo

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,15 @@ import CreateUser from "./components/create-user.component";
 function App() {
   return (
     <Router>
+      <div className = "container">
       <Navbar />
       <br/>
       {/* map url paths */}
-      <Route path="/" exact component={ExerciseList} />
+      <Route path="/" exact component={ExercisesList} />
       <Route path="/edit/:id" exact component={EditExercise} />
       <Route path="/create" exact component={CreateExercise} />
       <Route path="/user" exact component={CreateUser} />
-
+      </div>
     </Router>
   );
 }
