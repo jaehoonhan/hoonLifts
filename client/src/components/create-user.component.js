66c615de9a9481a76bf3d2c71b5b7992de035bf9
Jaehoon Han <JaehoonHan1@gmail.com>
2021-05-06 23:58:06 -0400

    add state change functions and form

--- client/src/components/create-user.component.js
+++ client/src/components/create-user.component.js
@@ -1,11 +1,60 @@
 import React, {Component} from 'react';
 
 export default class CreateUsers extends Component {
+    constructor(props) {
+        super(props);
+        // behind 'this' to each change state function
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        
+        // set initial state and properties
+        this.state = {
+            //for testing
+            username: '',
+        }
+    }
+    
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+    
+    onSubmit(e) {
+        // prevent default HTML submit behaviour
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username,
+        };
+        // for testing
+        console.log(user);
+
+        this.setState({
+            // for testing
+            username: ''
+        })
+    }
+
     render() {
         return (
-            <div>
-                <p>You are on the Create Users component!</p>
-            </div>
+        <div>
+            <h3>Create New User</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group"> 
+                <label>Username: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+                    />
+              </div>
+              <div className="form-group">
+                <input type="submit" value="Create User" className="btn btn-primary" />
+              </div>
+            </form>
+        </div>
         )
     }
 }
