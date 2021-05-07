3006eff1f24542947caeb311998a60e3bde49543
Jaehoon Han <JaehoonHan1@gmail.com>
2021-05-06 23:56:58 -0400

    missing semicolon

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from "./components/navbar.component"
+import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
