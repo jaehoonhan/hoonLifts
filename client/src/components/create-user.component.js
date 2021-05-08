7f98c2a396848f1ba2ea1bd940eab2528db7b369
Jaehoon Han <JaehoonHan1@gmail.com>
2021-05-07 22:12:29 -0400

    add frontend backend integration with axios

--- client/src/components/create-user.component.js
+++ client/src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class CreateUsers extends Component {
     constructor(props) {
@@ -29,6 +30,10 @@ export default class CreateUsers extends Component {
         };
         // for testing
         console.log(user);
+
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data));
+             
 
         this.setState({
             // for testing
